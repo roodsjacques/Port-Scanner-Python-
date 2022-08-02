a5f327a5b7c748f1e0baea879b373f7e935e6682
Roods Jacques <20774635+roodsjacques@users.noreply.github.com>
2022-08-02 18:37:25 -0400

    Update pscan.py

--- pscan.py
+++ pscan.py
@@ -1,7 +1,3 @@
-# Your task is to write a Python program that should test the given host and it should generate a
-# list of all the TCP Open ports within the range of 1 to 1025. You are required to accomplish this
-# task by using standard Python’s “socket” library.
-
 
 # PROJECT B
 # Port Scanner
